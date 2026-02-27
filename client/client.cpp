ac5483e78e60f07e6498cc95ca5ac97224eb32c6
Mumlul <al.ploskikh@list.ru>
2026-02-27 11:48:03 +0500

    v2

---
+++ client/client.cpp
@@ -0,0 +1,53 @@
+﻿#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    HANDLE hMutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, L"Global\\MyPrinterMutex");
+    HANDLE hEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"Global\\MyPrinterEvent");
+
+    if (!hMutex || !hEvent)
+    {
+        cout << "Принтер не найден!\n";
+        Sleep(2000);
+        return 1;
+    }
+
+    cout << "Выберите приоритет:\n";
+    cout << "1 - Низкий\n2 - Нормальный\n3 - Высокий\n";
+
+    int choice;
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1: SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS); break;
+    case 3: SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS); break;
+    default: SetPriorityClass(GetCurrentProcess(), NORMAL_PRIORITY_CLASS);
+    }
+
+    cout << "Отправка задания на печать...\n";
+
+    WaitForSingleObject(hMutex, INFINITE);
+
+    SetEvent(hEvent);  
+
+    ReleaseMutex(hMutex);
+
+    cout << "Ожидание завершения печати...\n";
+
+    WaitForSingleObject(hMutex, INFINITE);
+    ReleaseMutex(hMutex);
+
+    cout << "Печать завершена. Клиент закрывается.\n";
+
+    CloseHandle(hMutex);
+    CloseHandle(hEvent);
+
+    Sleep(2000);
+    return 0;
+}
---
+++ pract12_system.cpp
@@ -0,0 +1,68 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+
+    HANDLE hMutex = CreateMutex(NULL, FALSE, L"Global\\MyPrinterMutex");
+    HANDLE hEvent = CreateEvent(NULL, FALSE, FALSE, L"Global\\MyPrinterEvent");
+
+    if (!hMutex || !hEvent)
+    {
+        cout << "Ошибка создания объектов!\n";
+        return 1;
+    }
+
+    cout << "Принтер запущен.\n";
+    cout << "Ожидание клиента...\n";
+
+    DWORD lastActivity = GetTickCount64();
+
+    while (true)
+    {
+        DWORD result = WaitForSingleObject(hEvent, 1000);
+
+        if (result == WAIT_OBJECT_0)
+        {
+            WaitForSingleObject(hMutex, INFINITE);
+
+            cout << "\nКлиент подключён.\n";
+
+            srand((unsigned)time(NULL));
+            int printTime = 5 + rand() % 11;
+            cout << "Начало печати: " << printTime << " Секунд\n";
+
+            for (int i = 1; i <= printTime; i++)
+            {
+                cout << "Печать: " << i << " сек...\n";
+                Sleep(1000);
+            }
+
+            if (printTime > 10)
+                cout << "Ошибка печати! (более 10 сек)\n";
+            else
+                cout << "Печать успешно завершена.\n";
+
+            ReleaseMutex(hMutex);
+
+            cout << "Ожидание клиента...\n";
+
+            lastActivity = GetTickCount64();
+        }
+
+        if (GetTickCount64() - lastActivity > 600000)
+        {
+            cout << "\nНет клиентов 10 минут. Принтер выключается...\n";
+            break;
+        }
+    }
+
+    CloseHandle(hMutex);
+    CloseHandle(hEvent);
+    return 0;
+}
